198b8824a9cba8bb4cc9c6e1aa6f388a6eb764e5
rohang200303 <120368357+rohang200303@users.noreply.github.com>
2024-04-16 22:10:27 +0530

    Add files via upload

---
+++ amity/src/main/java/bca_bscit6/amity/App.java
@@ -0,0 +1,13 @@
+package bca_bscit6.amity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
